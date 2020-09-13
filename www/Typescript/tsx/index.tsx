c1a6e104bf4249452aa04513bc870e2266ffe72f
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-09-13 18:45:06 +0900

    バックエンド解説 #18

--- www/Typescript/tsx/index.tsx
+++ www/Typescript/tsx/index.tsx
@@ -7,45 +7,10 @@ var moment = require('moment');
 // IndexPage (Not use)
 const AppMain = () => {
     const [kensaku, setKensaku] = useState("")
-    const [kekka, setKekka] = useState("")
     const [total, setTotal] = useState(0)
     const [targetDataDate, setTargetDataDate] = useState("")
     const [respData, setRespData] = useState([])
 
-
-    const dataA =
-    {
-        '2018-12-01': { art: ["gras", "fas"] },
-        '2018-12-02': { art: ["gras", "fas"] },
-        '2018-12-03': { art: ["gra", "fas"] },
-        '2018-12-04': { art: ["gra", "fass"] },
-        '2018-12-05': { art: ["grva", "fas"] },
-        '2018-12-06': { art: ["gra", "fas"] },
-        '2018-12-07': { art: ["gra", "fas", "ffs"] },
-        '2018-12-08': { art: ["gra", "fasf"] },
-        '2018-12-09': { art: ["gra", "fas"] },
-        '2018-12-10': { art: ["gra", "fas"] },
-        '2018-12-11': { art: ["grsa", "fas"] },
-        '2018-12-12': { art: ["gra", "fas"] },
-        '2018-12-13': { art: ["gra", "fas"] },
-        '2018-12-14': { art: ["grfa", "fas"] },
-        '2018-12-15': { art: ["gra", "GE", "fas"] },
-        '2018-12-16': { art: ["gra", "fas"] },
-        '2018-12-17': { art: ["gra", "fas"] },
-        '2018-12-18': { art: ["gra", "fas"] },
-        '2018-12-19': { art: ["gra", "fas"] },
-        '2018-12-20': { art: ["grva", "fas"] },
-        '2018-12-21': { art: ["gra", "fas"] },
-        '2018-12-22': { art: ["gra", "fas"] },
-        '2018-12-23': { art: ["gra", "fas"] },
-        '2018-12-24': { art: ["gra", "fas"] },
-        '2018-12-25': { art: ["gra", "fas"] },
-        '2018-12-26': { art: ["grsa", "fas"] },
-        '2018-12-27': { art: ["gra", "fas"] },
-        '2018-12-28': { art: ["gra", "fass"] },
-        '2018-12-29': { art: ["gra", "fas"] },
-        '2018-12-30': { art: ["gra", "fas"] }
-    }
 
     const response2Data = (_xhrResponseText: any) => {
         const _respData = []
@@ -68,7 +33,7 @@ const AppMain = () => {
         xhr.onload = () => {
             if (xhr.readyState === 4 && xhr.status === 200) console.log(xhr.responseText);
             const resp: any = JSON.parse(xhr.responseText)
-            setKekka(resp["kekka"])
+            setRespData(response2Data(resp));
         };
         xhr.timeout = 5000;
         xhr.send(JSON.stringify({ "kensaku": kensaku }));
@@ -141,7 +106,6 @@ const AppMain = () => {
                     <button className="input-group-append btn btn-outline-primary btn-lg"
                         onClick={() => {
                             kensakusuruyo();
-                            setRespData(response2Data(dataA));
                         }}>
                         <i className="fas fa-search mr-1"></i>検索
                     </button>
--- www/sh2_api.py
+++ www/sh2_api.py
@@ -2,16 +2,44 @@ import flask
 from werkzeug.utils import secure_filename
 import json
 
+
 def show(request):
     return_dict = {}
     input_dict = json.loads(request.get_data())
-    print(request.get_data())
+
+    #仮の出力
+    data_kari = {
+        '2018-12-01': {"art": ["gras", "fas"]},
+        '2018-12-02': {"art": ["gras", "fas"]},
+        '2018-12-03': {"art": ["gra", "fas"]},
+        '2018-12-04': {"art": ["gra", "fass"]},
+        '2018-12-05': {"art": ["grva", "fas"]},
+        '2018-12-06': {"art": ["gra", "fas"]},
+        '2018-12-07': {"art": ["gra", "fas", "ffs"]},
+        '2018-12-08': {"art": ["gra", "fasf"]},
+        '2018-12-09': {"art": ["gra", "fas"]},
+        '2018-12-10': {"art": ["gra", "fas"]},
+        '2018-12-11': {"art": ["grsa", "fas"]},
+        '2018-12-12': {"art": ["gra", "fas"]},
+        '2018-12-13': {"art": ["gra", "fas"]},
+        '2018-12-14': {"art": ["grfa", "fas"]},
+        '2018-12-15': {"art": ["gra", "GE", "fas"]},
+        '2018-12-16': {"art": ["gra", "fas"]},
+        '2018-12-17': {"art": ["gra", "fas"]},
+        '2018-12-18': {"art": ["gra", "fas"]},
+        '2018-12-19': {"art": ["gra", "fas"]},
+        '2018-12-20': {"art": ["grva", "fas"]},
+        '2018-12-21': {"art": ["gra", "fas"]},
+        '2018-12-22': {"art": ["gra", "fas"]},
+        '2018-12-23': {"art": ["gra", "fas"]}, }
 
     if request.method == "POST":
         if 'kensaku' in input_dict:
-            return_dict["kekka"] = input_dict["kensaku"].translate(
-                str.maketrans("", "", "\"\'<>`;"))  # Not_secure_filename!
+            # 入力データ
+            input_dict["kensaku"].translate(str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
 
+            #出力データ
+            return_dict = data_kari
         return json.dumps(return_dict, ensure_ascii=False), 200
 
     return "ok", 200
