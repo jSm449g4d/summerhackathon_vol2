1d5f0b29136255db52b437f4c7eb8264e5303492
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-09-13 22:03:21 +0900

    【緊急】newsAPIのクライアントの設計 #41

--- www/sh2_api.py
+++ www/sh2_api.py
@@ -1,14 +1,16 @@
 import flask
 from werkzeug.utils import secure_filename
 import json
+import importlib
 
+newspy=importlib.import_module("news")
 
 def show(request):
     return_dict = {}
     input_dict = json.loads(request.get_data())
 
     #仮の出力
-    data_kari = {
+    data_kari_old = {
         '2018-12-01': {"art": ["gras", "fas"]},
         '2018-12-02': {"art": ["gras", "fas"]},
         '2018-12-03': {"art": ["gra", "fas"]},
@@ -35,11 +37,8 @@ def show(request):
 
     if request.method == "POST":
         if 'kensaku' in input_dict:
-            # 入力データ
-            input_dict["kensaku"].translate(str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
-
-            #出力データ
-            return_dict = data_kari
+            kensaku=input_dict["kensaku"].translate(str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
+            return_dict = newspy.push_news_data(kensaku)
         return json.dumps(return_dict, ensure_ascii=False), 200
 
     return "ok", 200
