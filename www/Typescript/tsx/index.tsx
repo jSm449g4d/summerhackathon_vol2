8f119fcbd7aa353e7a859cb142526746e2eeffd0
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-09-17 12:42:52 +0900

    フロントエンドのデザインについて #38

--- www/Typescript/tsx/index.tsx
+++ www/Typescript/tsx/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import './stylecheets/style.sass';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, LabelList } from 'recharts';
 import { AppWidgetHead, AppWidgetFoot } from "./components/widget";
-import { Unixtime2String } from "./components/util";
 
 
 // IndexPage (Not use)
@@ -15,6 +14,16 @@ const AppMain = () => {
     const [jpOnly, setJpOnly] = useState(true)
     const [message, setMessage] = useState("キーワードを入力してください")
 
+    const Unixtime2String = (unixtime: String = "0", yearFlag: Number = 1, mdFlag: Number = 1) => {
+        const now: Date = new Date(Number(unixtime));
+        let _timestamp = "";
+        if (yearFlag == 1) { _timestamp += now.getFullYear() + "年 " }
+        if (mdFlag == 1) {
+            _timestamp += String(now.getMonth() + 1) +
+                "月 " + now.getDate() + "日 " + now.getHours() + ": " + now.getMinutes() + ": " + now.getSeconds();
+        }
+        return _timestamp
+    }
     const response2Data = (_xhrResponseText: any) => {
         const sortRespData = (a: any, b: any) => {
             return Number(String(a.date)) - Number(String(b.date));
@@ -26,10 +35,10 @@ const AppMain = () => {
         setMessage("")
         for (let i = 0; i < _len; i++) {
             const _value = _resp[Object.keys(_resp)[i]]//{"description":"aaa","url":"bbb"},...
-            _respData.push({ date: Unixtime2String(Number(Object.keys(_resp)[i])), num: _value.length, arts: _value })
+            _respData.push({ date: Object.keys(_resp)[i], num: _value.length, arts: _value })
         }
         _respData.sort(sortRespData)
-        setTargetDataDate(Unixtime2String(Number(Object.keys(_resp)[0])))
+        setTargetDataDate(String(_respData[_len - 1].date))
         return _respData
     }
     const kensakusuruyo = () => {
@@ -54,7 +63,10 @@ const AppMain = () => {
         });
         if (datum.length != 1) { return (<div></div>) }
         const _datails = [];
-        _datails.push(<h4 className="col-12 p-1 text-center" style={{ backgroundColor: "rgba(250,250,250,0.8)" }}>{targetDataDate}の時間帯の記事</h4>)
+        _datails.push(
+            <h4 className="col-12 p-1 text-center" style={{ backgroundColor: "rgba(250,250,250,0.8)" }}>
+                {Unixtime2String(String(targetDataDate))}の時間帯の記事
+            </h4>)
         for (let i = 0; i < datum[0].arts.length; i++) {
             _datails.push(
                 <div className="col-12 col-lg-6 p-1">
@@ -83,12 +95,11 @@ const AppMain = () => {
         return (
             <ResponsiveContainer width={'99%'} height={300}>
                 <BarChart width={400} height={400} data={respData}>
-                    <XAxis dataKey="date" />
+                    <XAxis dataKey="date" tickFormatter={(tick) => Unixtime2String(String(tick), 0)} />
                     <YAxis dataKey="num" label={{ value: 'Number of articles', angle: -90, position: 'insideLeft' }} />
                     <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-                    <Tooltip />
                     <Bar dataKey="num" fill="#8884d8"
-                        onMouseEnter={(evt) => { setTargetDataDate(evt.date) }} >
+                        onMouseEnter={(evt) => { setTargetDataDate(String(evt.date)) }} >
                         <LabelList dataKey="num" position="top" />
                     </Bar>
                 </BarChart>
@@ -102,9 +113,9 @@ const AppMain = () => {
                 <h4 className="text-center m-1 text-center">
                     期間
                     <div className="row">
-                        <div className="col-12 col-lg-5">{respData[0].date}</div>
+                        <div className="col-12 col-lg-5">{Unixtime2String(respData[0].date)}</div>
                         <div className="d-none d-lg-block col-lg-2">-</div>
-                        <div className="col-12 col-lg-5">{respData[respData.length - 1].date}</div>
+                        <div className="col-12 col-lg-5">{Unixtime2String(respData[respData.length - 1].date)}</div>
                     </div>
                 </h4>)
         return (<div></div>)
@@ -152,7 +163,7 @@ const AppMain = () => {
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="index_jponly_check"
                             defaultChecked={jpOnly} checked={jpOnly} onChange={evt => setJpOnly(!jpOnly)} />
-                        <label style={{ color: "darkcyan" }} htmlFor="index_jponly_check"><h5>日本語検索のみ</h5></label>
+                        <label style={{ color: "darkcyan" }} htmlFor="index_jponly_check"><h5>日本語記事のみ</h5></label>
                     </div>
                 </div>
                 <div className="input-group">
